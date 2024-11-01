da8e2e076a0ee56b83517461217f6165b3fbaf53
DCampbell121 <dcampbell121@yahoo.com>
2024-11-01 12:35:58 -0500

    Added my wall follow as alt_wall_follow

--- wander.py
+++ wander.py
@@ -46,3 +46,37 @@ def wall_follow(drive_base=DriveBase,c_sensor=ColorSensor,s_ultra=UltrasonicSens
         if c_sensor.color(surface=True) == Color.GREEN:
             goal_found(fan_motor)
             break
+
+def alt_wall_follow(drive_base=DriveBase,c_sensor=ColorSensor,s_ultra=UltrasonicSensor,f_ultra=UltrasonicSensor, fan_motor=Motor):
+    #follow a wall on the robots left side
+    drive_base.drive()
+    goal = False
+    next_to_wall = True
+    dist_increment = 250 #how far the robot will go every cycle to look for a wall in front or adjust to straighten along the wall to the left
+    
+    #drive forward the dist_increment, if goal is found then end, if wall no longer there then wander
+    #else get new distance to wall and try to calculate the angle to straighten along
+    while not goal and next_to_wall:
+        front_wall_dist = f_ultra.distance
+        
+        #if the wall is too close we need to turn right
+        if front_wall_dist <= dist_increment:
+            drive_base.turn(90)            
+            
+        left_wall_dist = s_ultra.distance
+        drive_base.straight(distance=dist_increment) #only goes in 250mm stretches, can increase
+        new_wall_dist = s_ultra.distance
+        if c_sensor.color(surface=True) == Color.GREEN:
+            goal = True
+        elif new_wall_dist > 100: #wall no longer found
+            next_to_wall = False
+        else:
+            #adjust angle to try to drive parallel to the wall
+            dist_difference = new_wall_dist - left_wall_dist
+            adjust_angle = umath.atan(dist_difference / 250) #debated on acos here?
+            drive_base.turn(adjust_angle)
+        
+    if goal:
+        goal_found(fan_motor)
+    else:
+        wander(drive_base, c_sensor, s_ultra, f_ultra, fan_motor)
