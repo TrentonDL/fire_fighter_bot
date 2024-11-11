b0cd44753dbde0ca4aee48a4a2d68d2a987afc4f
TrentonDL <trenton.laule@mavs.uta.edu>
2024-11-11 15:02:12 -0600

    edit things to get it working

--- main_bot.py
+++ main_bot.py
@@ -1,3 +1,5 @@
+# run using ---->     pybricksdev run ble --name  "Team5"  main_bot.py
+
 from pybricks.hubs import PrimeHub
 from pybricks.parameters import Axis
 from pybricks.pupdevices import Motor,ColorSensor,UltrasonicSensor
@@ -33,12 +35,12 @@ def wander_area(drive_base=DriveBase,c_sensor=ColorSensor,s_ultra=UltrasonicSens
         
         drive_base.turn(randint(10,90)) #rand turn dist from 10 - 90 degrees to the right
         
-        if s_ultra.distance < 220:
+        if s_ultra.distance() < 220:
             wall_follow(drive_base=DriveBase,c_sensor=ColorSensor,s_ultra=UltrasonicSensor,f_ultra=UltrasonicSensor, fan_motor=Motor)
 
 def wall_follow(drive_base=DriveBase,c_sensor=ColorSensor,s_ultra=UltrasonicSensor,f_ultra=UltrasonicSensor, fan_motor=Motor):
     # Start moving forward
-    drive_base.drive(SPEED_RATE, 0) 
+    drive_base.drive(drive_base,SPEED_RATE, 0) 
     while not GOAL:
         # Measure distance to the wall on the left side
         s_dist = s_ultra.distance()
@@ -66,19 +68,19 @@ def alt_wall_follow(drive_base=DriveBase,c_sensor=ColorSensor,s_ultra=Ultrasonic
     #follow a wall on the robots left side
     drive_base.drive(SPEED_RATE,0)
     next_to_wall = True
-    dist_increment = int(250) #how far the robot will go every cycle to look for a wall in front or adjust to straighten along the wall to the left
+    dist_increment = int(100) #how far the robot will go every cycle to look for a wall in front or adjust to straighten along the wall to the left
     
     #drive forward the dist_increment, if goal is found then end, if wall no longer there then wander
     #else get new distance to wall and try to calculate the angle to straighten along
     while not GOAL and next_to_wall:
-        front_wall_dist = f_ultra.distance
+        front_wall_dist = f_ultra.distance()
         #if the wall is too close we need to turn right
         if front_wall_dist <= dist_increment:
             drive_base.turn(90)            
         else:    
-            left_wall_dist = s_ultra.distance
+            left_wall_dist = s_ultra.distance()
             drive_base.straight(distance=dist_increment,wait=True)
-            new_wall_dist = s_ultra.distance
+            new_wall_dist = s_ultra.distance()
             if check_goal(c_sensor):
                 break
             if new_wall_dist > 100: #wall no longer found
@@ -86,11 +88,12 @@ def alt_wall_follow(drive_base=DriveBase,c_sensor=ColorSensor,s_ultra=Ultrasonic
             else:
                 #adjust angle to try to drive parallel to the wall
                 dist_difference = new_wall_dist - left_wall_dist
-                adjust_angle = umath.atan(dist_difference / 250) #debated on acos here?
-                drive_base.turn(SPEED_RATE,adjust_angle)
+                adjust_angle = umath.atan2(dist_difference,float(250)) #debated on acos here?
+                adjust_angle = umath.degrees(adjust_angle)
+                drive_base.turn(adjust_angle)
         
     if GOAL:
-        goal_found(fan_motor)
+        goal_found(drive_base,fan_motor)
     else:
         wander_area(drive_base, c_sensor, s_ultra, f_ultra, fan_motor)
 
@@ -106,20 +109,30 @@ def check_goal(c_sensor):
             GOAL = True
     return GOAL
 
+def clean_Motors(lMotor=Motor,rMotor=Motor,fMotor=Motor,cSenor=ColorSensor,sUltra=UltrasonicSensor,fUltra=UltrasonicSensor):
+    lMotor.close()
+    rMotor.close()
+    fMotor.close()
+    cSenor.lights.off()
+    sUltra.lights.off()
+    fUltra.lights.off()
+
 def main():
+
+    l_Motor = Motor(Port.A,Direction.COUNTERCLOCKWISE,reset_angle=True)
+    r_Motor = Motor(Port.B, Direction.CLOCKWISE, reset_angle=True)
+    fan_motor = Motor(Port.C, Direction.CLOCKWISE, reset_angle=True)
+    color_sensor = ColorSensor(Port.E)
+    side_ultra_sonic = UltrasonicSensor(Port.F)
+    front_ultra_sonic = UltrasonicSensor(Port.D)
+
     try:
         #initialize hub and reset IMU
         hub = PrimeHub(top_side=Axis.Z, front_side=Axis.X)
         hub.imu.reset_heading(0)
 
         #Define All Motors and sensors
-        l_Motor = Motor(Port.A,Direction.COUNTERCLOCKWISE,reset_angle=True)
-        r_Motor = Motor(Port.B, Direction.CLOCKWISE, reset_angle=True)
-        fan_motor = Motor(Port.C, Direction.CLOCKWISE, reset_angle=True)
-        color_sensor = ColorSensor(Port.E)
-        side_ultra_sonic = UltrasonicSensor(Port.F)
-        front_ultra_sonic = UltrasonicSensor(Port.D)
-
+        
         drive_base = DriveBase(l_Motor,r_Motor,wheel_diameter=55.5, axle_track=127)
         drive_base.use_gyro(True)
 
@@ -129,12 +142,6 @@ def main():
     finally:
         clean_Motors(l_Motor,r_Motor,fan_motor,color_sensor,side_ultra_sonic,front_ultra_sonic)
         
-def clean_Motors(lMotor=Motor,rMotor=Motor,fMotor=Motor,cSenor=ColorSensor,sUltra=UltrasonicSensor,fUltra=UltrasonicSensor):
-    lMotor.close()
-    rMotor.close()
-    fMotor.close()
-    cSenor.lights.off()
-    sUltra.lights.off()
-    fUltra.lights.off()
+
 
 main()
